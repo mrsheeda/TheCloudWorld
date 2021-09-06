d5f14816540e61e89484299b752a90977e8e7152
wilshan <74950445+wilshan@users.noreply.github.com>
2021-09-06 09:21:57 +0000

    Add files via upload

---
+++ src/main/java/com/example/webapp1/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.webapp1.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping("/")
+	public String sayHello() {
+		return "Hello, This app is created for Demo purpose to NYL";
+	}
+
+}
